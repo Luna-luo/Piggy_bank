73100b96b12599073e77133b47585c774fa7d9c3
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-08-09 16:46:52 +0800

    将tag的string改为{id,name}

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,8 +16,10 @@
   import Tags from '@/components/Money/Tags.vue';
   import {Component,Watch} from 'vue-property-decorator';
   import recordListModel from '@/models/recordListModel';
+  import tagListModel from '@/models/tagListModel';
 
   const recordList = recordListModel.fetch();
+  const tagList = tagListModel.fetch();
 
 
 
@@ -25,7 +27,7 @@
     components:{Tags,Notes,Types,NumberPad}
   })
   export default class Money extends Vue {
-    tags = ['衣','食','住','行','彩票'];
+    tags = tagList;
     recordList: RecordItem[]= recordList;
     record: RecordItem={
       tags:[],notes:'',type:'-',amount:0
