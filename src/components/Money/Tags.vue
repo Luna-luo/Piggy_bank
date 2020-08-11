f51694d8e65147544fb65fedbb2dd7dc81317362
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-08-11 15:16:45 +0800

    Money.vue使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -15,11 +15,17 @@
 <script lang="ts">
   import {Component} from 'vue-property-decorator';
   import Vue from 'vue';
-  import store from '@/store/index2';
 
-  @Component
+  @Component({
+    computed:{
+      tagList(){
+        //TODO
+        //return this.$store.fetchTags();
+        return []
+      }
+    }
+  })
   export default class Tags extends Vue{
-    tagList = store.fetchTags();
     selectedTags: string[]=[];
 
     toggle(tag:string){
@@ -35,7 +41,8 @@
     create() {
       const name = window.prompt('请输入标签名');
       if(!name){return window.alert('标签名不能为空');}
-      store.createTag(name);
+      //TODO
+      // store.createTag(name);
     }
   }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,7 +20,6 @@
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
   import FormItem from '@/components/Money/FormItem.vue';
-  import oldStore from '@/store/index2';
 
 
 
@@ -28,23 +27,24 @@
     components:{FormItem, Tags, Types,NumberPad},
     computed:{
       recordList(){
-        return this.$store.state.count;
+        return this.$store.state.recordList;
       }
     }
   })
   export default class Money extends Vue {
-    recordList = oldStore.recordList;
     record: RecordItem={
       tags:[],notes:'',type:'-',amount:0
     };
-
+    created(){
+      this.$store.commit('fetchRecords')
+    }
 
     onUpdateNotes(value:string){
       this.record.notes = value;
     }
 
     saveRecord(){
-      oldStore.createRecord(this.record);
+      this.$store.commit('createRecord',this.record);
     }
   }
 </script>
