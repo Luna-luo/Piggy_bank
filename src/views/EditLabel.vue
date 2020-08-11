fc3e790a6cccc108e25fd8aa3364bd5570ab438c
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-08-11 16:30:56 +0800

    EditLabel.vue使用Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -24,14 +24,16 @@
     import Button from '@/components/Button.vue';
 
     @Component({
-      components:{Button, FormItem}
+      components:{Button, FormItem},
     })
     export default class EditLabel extends Vue{
-      tag?:Tag = undefined;
+      get tag(){
+        return this.$store.state.currentTag;
+      }
 
         created(){
-          // this.tag = store.findTag(this.$route.params.id);
-          //todo
+        const id = this.$route.params.id;
+        this.$store.commit('setCurrentTag',id);
           if(!this.tag){
             this.$router.replace('/404');
           }
@@ -51,7 +53,7 @@
             //   window.alert('删除失败');
             // }
             //todo
-            return
+            return;
           }
         }
         goBack(){
