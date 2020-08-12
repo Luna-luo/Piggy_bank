55af62bb3dce2063a496f60aff361b30f53d17ef
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-08-12 17:32:44 +0800

    更新CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -20,21 +20,25 @@
       }
     }
   }
+  ::v-deep .interval-tabs-item {
+    height:48px;
+  }
 
 </style>
 
 <script lang="ts">
   import Vue from 'vue';
-  import Types from '@/components/Money/Types.vue';
   import Tabs from '@/components/Tabs.vue';
   import {Component} from 'vue-property-decorator';
+  import recordTypeList from '@/constants/recordTypeList';
 
   @Component({
-    components:{Tabs,Types},
+    components:{Tabs},
   })
   export default class Statistics extends Vue{
     type='-';
     interval='day';
+    recordTypeList = recordTypeList;
   }
 </script>
 
