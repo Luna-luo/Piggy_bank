1385f9addc30796dfdcd82cb194accfd2c1a0a22
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-08-20 17:03:21 +0800

    重构代码

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -41,9 +41,9 @@
       return tags.length === 0 ? '无' : tags.map(t=>t.name).join('，');
     }
     mounted(){
-      console.log('Chart');
-      console.log(Chart);
-      (this.$refs.chartWrapper as HTMLDivElement).scrollLeft = 9999;
+      const div=this.$refs.chartWrapper as HTMLDivElement;
+      console.log(div.scrollWidth);
+      div.scrollLeft=div.scrollWidth;
     }
 
     beautify(string:string){
